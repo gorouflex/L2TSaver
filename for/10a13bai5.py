bd6b3ef747cab3202536f4a3039009dd9a6ffe89
GorouFlex <98001973+gorouflex@users.noreply.github.com>
2025-02-19 21:19:07 +0700

    Update 10a13bai5.py

--- for/10a13bai5.py
+++ for/10a13bai5.py
@@ -1,5 +1,10 @@
+## Chuong trinh tinh tong S = 1 + 1/2 + 1/3 + ... + 1/n
 n = int(input("Nhap so nguyen duong n: "))
 S = 0
 for i in range(1, n+1):
-    S = S+1/i
+    S = S + 1 / i
 print("KQ:", S)
+
+## Vi du:
+# Nhap: n = 3
+# Xuat: KQ: 1.8333 (1 + 1/2 + 1/3 = 1.8333)
