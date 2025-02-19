c7dac0b1988468b6bda03097d0307b4814438d1c
GorouFlex <98001973+gorouflex@users.noreply.github.com>
2025-02-19 21:18:06 +0700

    Update 10a13bai2.py

--- for/10a13bai2.py
+++ for/10a13bai2.py
@@ -1,6 +1,11 @@
+## Chuong trinh tinh tong cac so chan tu 1 den n
 n = int(input("Nhap so nguyen duong n: "))
 S = 0
 for i in range(1, n+1):
-    if i%2==0:
-        S = S+i
-print("Ket qua: ",S)
+    if i % 2 == 0:
+        S = S + i
+print("Ket qua:", S)
+
+## Vi du:
+# Nhap: n = 6
+# Xuat: Ket qua: 12 (2 + 4 + 6 = 12)
