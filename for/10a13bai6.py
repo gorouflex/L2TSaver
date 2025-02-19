eaaf59eb2f06a943c5c986fba93b9deda71030eb
GorouFlex <98001973+gorouflex@users.noreply.github.com>
2025-02-19 21:19:17 +0700

    Update 10a13bai6.py

--- for/10a13bai6.py
+++ for/10a13bai6.py
@@ -1,5 +1,10 @@
+## Chuong trinh tinh tong lap phuong cac so tu 1 den n
 n = int(input("Nhap so nguyen duong n: "))
 S = 0
 for i in range(1, n+1):
-    S = S+i*i*i	
+    S = S + i * i * i
 print("KQ:", S)
+
+## Vi du:
+# Nhap: n = 3
+# Xuat: KQ: 36 (1^3 + 2^3 + 3^3 = 1 + 8 + 27 = 36)
