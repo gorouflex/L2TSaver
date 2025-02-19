b8418c57517f492d08f209494fd5b0d182ae2c5b
GorouFlex <98001973+gorouflex@users.noreply.github.com>
2025-02-19 21:18:29 +0700

    skibidi dop dop yes

--- for/10a13bai3.py
+++ for/10a13bai3.py
@@ -1,6 +1,11 @@
+## Chuong trinh dem so luong uoc cua n
 n = int(input("Nhap so nguyen duong n: "))
 dem = 0
 for i in range(1, n+1):
-    if n%i==0:
-        dem = dem +1
+    if n % i == 0:
+        dem = dem + 1
 print("So luong cua uoc cua n:", dem)
+
+## Vi du:
+# Nhap: n = 6
+# Xuat: So luong cua uoc cua n: 4 (1, 2, 3, 6)
