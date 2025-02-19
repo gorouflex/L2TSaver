01347c4bf3d406ed1f12b4149c9b838ae89df0da
GorouFlex <98001973+gorouflex@users.noreply.github.com>
2025-02-19 21:35:59 +0700

    oppsie

--- while/10a13bai6.py
+++ while/10a13bai6.py
@@ -1,17 +1,16 @@
-## Chuong trinh tim BCNN cua hai so a va b
-a = int(input("Nhap a:"))
-b = int(input("Nhap b:"))
-m = 0
-if a>b:
-    m = a
-else:
-    m = b
-MAX = 0
-for i in range(1, m+1):
-    if a%i==0 and b%i==0:
+## Chuong trinh tim BCNN cua hai so a va b su dung vong lap while
+
+a = int(input("Nhap a: "))
+b = int(input("Nhap b: "))
+i = a if a < b else b 
+MAX = 1 
+while i > 0:
+    if a % i == 0 and b % i == 0:
         MAX = i
-BCNN = a*b/MAX
+    i = i - 1 
+BCNN = a * b // MAX
 print("BCNN:", BCNN)
+
 ## Vi du:
 # Nhap: a = 12, b = 18
 # Xuat: BCNN: 36
