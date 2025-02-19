06a66364238ccd18cd8a74215a8338d63ca93328
GorouFlex <98001973+gorouflex@users.noreply.github.com>
2025-02-19 21:18:49 +0700

    Update 10a13bai4.py

--- for/10a13bai4.py
+++ for/10a13bai4.py
@@ -1,9 +1,14 @@
+## Chuong trinh tinh trung binh cong cac uoc cua n
 n = int(input("Nhap so nguyen duong n: "))
 S = 0
 dem = 0
 for i in range(1, n+1):
-    if n%i==0:
+    if n % i == 0:
         S = S + i
-        dem = dem+1
-tbc = S/dem
+        dem = dem + 1
+tbc = S / dem
 print("TBC:", tbc)
+
+## Vi du:
+# Nhap: n = 6
+# Xuat: TBC: 3.0 ((1 + 2 + 3 + 6) / 4 = 3.0)
