c214672aa2dc10f60f2655ac4408f6b0c61e9ab1
GorouFlex <98001973+gorouflex@users.noreply.github.com>
2025-02-19 21:43:48 +0700

    fix

--- while/10a13bai6.py
+++ while/10a13bai6.py
@@ -2,14 +2,19 @@
 
 a = int(input("Nhap a: "))
 b = int(input("Nhap b: "))
-i = a if a < b else b 
-MAX = 1 
-while i > 0:
+
+m = a if a > b else b
+i = 1
+MAX = 1
+
+while i <= m:
     if a % i == 0 and b % i == 0:
         MAX = i
-    i = i - 1 
-BCNN = a * b / MAX
+    i += 1
+
+BCNN = a * b // MAX
 print("BCNN:", BCNN)
+
 
 ## Vi du:
 # Nhap: a = 12, b = 18
