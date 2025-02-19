e729c4c7cd113f968d76fa69ebd82034f17302cc
GorouFlex <98001973+gorouflex@users.noreply.github.com>
2025-02-19 21:44:51 +0700

    fix

--- while/10a13bai5.py
+++ while/10a13bai5.py
@@ -1,15 +1,15 @@
 ## Chuong trinh tim UCLN cua hai so a va b
-a = int(input("Nhap a:"))
-b = int(input("Nhap b:"))
-m = 0
-if a>b:
-    m = a
-else:
-    m = b
-MAX = 0
-for i in range(1, m+1):
-    if a%i==0 and b%i==0:
+a = int(input("Nhap a: "))
+b = int(input("Nhap b: "))
+
+m = a if a > b else b
+i = 1
+MAX = 1
+
+while i <= m:
+    if a % i == 0 and b % i == 0:
         MAX = i
+    i += 1
 print("UCLN:", MAX)
 
 ## Vi du:
