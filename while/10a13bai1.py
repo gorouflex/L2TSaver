2d173c13261d43bdbcbaeb58e46c3bcc2dc7692e
GorouFlex <98001973+gorouflex@users.noreply.github.com>
2025-02-19 21:20:32 +0700

    Update 10a13bai1.py

--- while/10a13bai1.py
+++ while/10a13bai1.py
@@ -1,6 +1,10 @@
+## Chuong trinh tinh tong cac so chan tu 2 den 100
 S = 0
 i = 2
-while i<=100:
+while i <= 100:
     S = S + i
-    i = i+2
-print("Tong: ",S)
+    i = i + 2
+print("Tong:", S)
+
+## Vi du:
+# Xuat: Tong: 2550 (2 + 4 + ... + 100)
