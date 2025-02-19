98a180c83aecf933aa7603983da7dff00a5bdac4
GorouFlex <98001973+gorouflex@users.noreply.github.com>
2025-02-19 21:21:34 +0700

    Update 10a13bai2.py

--- while/10a13bai2.py
+++ while/10a13bai2.py
@@ -1,5 +1,9 @@
+## Chuong trinh tim so lon nhat nho hon 100 va chia het cho 3
 i = 1
 while i < 100:
-    i = i+3
-i = i-3
-print("Max: ", i)
+    i = i + 3
+i = i - 3
+print("Max:", i)
+
+## Vi du:
+# Xuat: Max: 99 (so lon nhat < 100 chia het cho 3)
