5c057384cc098d1cee18a455b9bc100dd2a95b67
GorouFlex <98001973+gorouflex@users.noreply.github.com>
2025-02-19 21:21:45 +0700

    Update 10a13bai3.py

--- while/10a13bai3.py
+++ while/10a13bai3.py
@@ -1,7 +1,11 @@
+## Chuong trinh dem cac so tu 0 den 100 chia het cho 5 hoac chia 3 du 1
 i = 0
 s = 0
 while i <= 100:
-    if i%5==0 or i%3==1:
-        s = s+1
-    i = i+1
+    if i % 5 == 0 or i % 3 == 1:
+        s = s + 1
+    i = i + 1
 print(s)
+
+## Vi du:
+# Xuat: 68 (so luong cac so thoa man dieu kien)
