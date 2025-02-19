70a672771147d23104834c801d7bd9e0e8edd1e9
GorouFlex <98001973+gorouflex@users.noreply.github.com>
2025-02-19 21:25:39 +0700

    Update 10a13bai4.py

--- while/10a13bai4.py
+++ while/10a13bai4.py
@@ -1,4 +1,12 @@
+## Chuong trinh in bang so tu 1 den 100 theo dinh dang 10x10
 for i in range(10):
     for j in range(1, 11):
-         print(i*10+j, end=" ")
+        print(i * 10 + j, end=" ")
     print("\n")
+
+## Vi du:
+# Xuat:
+#  1  2  3  4  5  6  7  8  9 10
+# 11 12 13 14 15 16 17 18 19 20
+# ...
+# 91 92 93 94 95 96 97 98 99 100
