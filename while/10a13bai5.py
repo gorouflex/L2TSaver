3b4355dd5273cb6d614e7df12ff94d4f381845b3
GorouFlex <98001973+gorouflex@users.noreply.github.com>
2025-02-19 21:26:50 +0700

    d=

--- while/10a13bai5.py
+++ while/10a13bai5.py
@@ -1,3 +1,4 @@
+## Chuong trinh tim UCLN cua hai so a va b
 a = int(input("Nhap a:"))
 b = int(input("Nhap b:"))
 m = 0
@@ -10,3 +11,7 @@ for i in range(1, m+1):
     if a%i==0 and b%i==0:
         MAX = i
 print("UCLN:", MAX)
+
+## Vi du:
+# Nhap: a = 12, b = 18
+# Xuat: UCLN: 6
