0d839413c8495e7887dfe375ce5f1060b9b0f9f2
GorouFlex <98001973+gorouflex@users.noreply.github.com>
2025-03-15 22:28:26 +0700

    LAST TIME I'M DOING THIS FOR GOOD

--- list/10a13baiSNT.py
+++ list/10a13baiSNT.py
@@ -5,7 +5,7 @@ n = int(input("Nhap so nguyen duong n: "))
 d = 0
 for i in range(1, n+1):
     if n % i == 0:
-        d += 1
+        d = d + 1
 if d == 2:
     print(n, "la so nguyen to")
 else:
@@ -16,7 +16,7 @@ for i in range(2, n+1):
     d = 0
     for j in range(1, i+1):
         if i % j == 0:
-            d += 1
+            d = d + 1
     if d == 2:
         print(i, end=" ")
 
@@ -28,7 +28,7 @@ for i in range(2, n+1):
     d = 0
     for j in range(1, i+1):
         if i % j == 0:
-            d += 1
+            d = d + 1
     if d == 2:
         tong += i
         dem += 1
